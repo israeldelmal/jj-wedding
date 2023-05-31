9bd36d9e0c27c18ae97818382f05de81bec46997
israeldelmal <israelofevil@gmail.com>
2023-05-31 15:34:27 -0600

    To update version

--- src/app/pages/j-a-j/j-a-j.module.ts
+++ src/app/pages/j-a-j/j-a-j.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 
+import { TranslateModule } from '@ngx-translate/core'
+
 import { JAJComponent } from './j-a-j.component'
 import { JAJRoutingModule } from './j-a-j-routing.module'
 
 @NgModule({
 	declarations: [JAJComponent],
-	imports: [CommonModule, JAJRoutingModule],
+	imports: [CommonModule, JAJRoutingModule, TranslateModule],
 })
 export class JAJModule {}
