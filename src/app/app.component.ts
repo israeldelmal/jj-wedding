194c40e1ddb3345da809006dd6873200d27b4cfc
israeldelmal <israelofevil@gmail.com>
2023-07-10 18:50:30 -0600

    Final project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DOCUMENT } from '@angular/common'
 import { Component, OnInit, inject } from '@angular/core'
 
 import { TranslateService } from '@ngx-translate/core'
@@ -8,10 +9,12 @@ import { TranslateService } from '@ngx-translate/core'
 })
 export class AppComponent implements OnInit {
 	private readonly translateService = inject(TranslateService)
+	private readonly document = inject(DOCUMENT)
 
 	ngOnInit(): void {
 		const browserLang = this.translateService.getBrowserLang()
 
+		this.document.documentElement.lang = browserLang
 		this.translateService.addLangs(['es', 'en'])
 		this.translateService.setDefaultLang(browserLang)
 		this.translateService.use(browserLang)
